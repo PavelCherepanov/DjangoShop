86b88f7d3778ea69d5b24a9c02c58b2213679600
MarinaSivodedova <sivo.m@yandex.ru>
2022-11-23 00:32:49 +0700

    Category, Brand, Color

--- management/models.py
+++ management/models.py
@@ -22,7 +22,7 @@ class CategoryManagement(models.Model):
 
 class ColorManagement(models.Model):
     color_name = models.CharField(max_length=200, verbose_name='Цвет')
-
+    color_name_value = models.CharField(max_length=6, verbose_name='Тип', null=True)
     def __str__(self):
         return self.color_name
 
@@ -42,13 +42,14 @@ class BrandManagement(models.Model):
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200, verbose_name='Имя')
-    product_price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена')
-    product_description = models.CharField(max_length=200, verbose_name='Описание', blank=True)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    product_description = models.CharField(max_length=400, verbose_name='Описание', blank=True)
     product_img = models.ImageField(upload_to='product_img/')
     product_status = models.ForeignKey(StatusManagement, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
     product_category = models.ForeignKey(CategoryManagement, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Категория')
     product_color = models.ForeignKey(ColorManagement, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Цвет')
     product_brand = models.ForeignKey(BrandManagement, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Бренд')
+    product_img_hover = models.ImageField(upload_to='product_img/', default='-')
     def __str__(self):
         return self.product_name
 
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from management.models import Product
+from management.models import Product, CategoryManagement, BrandManagement, ColorManagement
 
 
-# Create your views here.
-# Это сделала Марина
-# Павелк точка Черепан
+
 def shop_page(request):
     products_list = Product.objects.all()
-    dict_object = {'products_list': products_list}
+    CategoryManagement_list = CategoryManagement.objects.all()
+    BrandManagement_list = BrandManagement.objects.all()
+    ColorManagement_list = ColorManagement.objects.all()
+    dict_object = {'products_list': products_list, 'CategoryManagement_list': CategoryManagement_list, 'BrandManagement_list': BrandManagement_list, 'ColorManagement_list': ColorManagement_list}
     return render(request, 'shop.html', dict_object)
