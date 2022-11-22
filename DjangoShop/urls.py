c1d01f989666b1c9d247afcc166bf3e5635dd126
MarinaSivodedova <sivo.m@yandex.ru>
2022-11-22 16:05:05 +0700

    urls shop

--- DjangoShop/urls.py
+++ DjangoShop/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from accounts import activate
+from shop.views import shop_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +17,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('activate/<slug:uidb64>/<slug:token>/', activate.activate, name='activate'),
+    path('shop/', shop_page),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
