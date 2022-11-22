2bd0c790b0ab8e2393edbfa20a64668507985f53
MarinaSivodedova <sivo.m@yandex.ru>
2022-11-22 13:25:18 +0700

    Add 22/11

--- DjangoShop/urls.py
+++ DjangoShop/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from accounts import activate
-from shop.views import  shop_page
+from shop.views import shop_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from management.models import Product
+
 
 # Create your views here.
 # Это сделала Марина
 # Павелк точка Черепан
+def shop_page(request):
+    products_list = Product.objects.all()
+    dict_object = {'products_list': products_list}
+    return render(request, 'shop.html', dict_object)
