c028242d460a3bda132d8c1128d229c681a23676
MarinaSivodedova <sivo.m@yandex.ru>
2022-11-24 12:55:05 +0700

    Filter category and color

--- DjangoShop/urls.py
+++ DjangoShop/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+import shop
 from . import views
 from accounts import activate
-from shop.views import shop_page
+from shop.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,7 +19,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('activate/<slug:uidb64>/<slug:token>/', activate.activate, name='activate'),
-    path('shop/', shop_page),
+    path('filter_category/<str:parameter>/<int:id>', filter_category, name="filter_category"),
+    path('shop/', shop_page)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -10,3 +10,13 @@ def shop_page(request):
     ColorManagement_list = ColorManagement.objects.all()
     dict_object = {'products_list': products_list, 'CategoryManagement_list': CategoryManagement_list, 'BrandManagement_list': BrandManagement_list, 'ColorManagement_list': ColorManagement_list}
     return render(request, 'shop.html', dict_object)
+
+def filter_category(request, parameter, id):
+    #parameter = 'product_category'
+    #id = 1
+    products_list = Product.objects.filter(**{parameter: id})
+    CategoryManagement_list = CategoryManagement.objects.all()
+    BrandManagement_list = BrandManagement.objects.all()
+    ColorManagement_list = ColorManagement.objects.all()
+    dict_object = {'products_list': products_list, 'CategoryManagement_list': CategoryManagement_list, 'BrandManagement_list': BrandManagement_list, 'ColorManagement_list': ColorManagement_list}
+    return render(request, 'shop.html', dict_object)
